0df702f2951bcd003cce22e6ffa2e0a433b7d336
root <root@LAPTOP-8251PJTL.localdomain>
2022-12-05 17:19:11 +0530

    my first wether app

---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template, request
+import json
+import requests 
+import urllib.request
+
+app = Flask(__name__) #creating the Flask class object   
+
+@app.route('/', methods = ['POST','GET']) #decorator   
+
+
+def home():  
+
+    if request.method == 'POST':
+        city = request.form['city']
+        print(city)
+    else:
+        city = 'bangalore'
+
+
+    url = f" https://api.openweathermap.org/data/2.5/weather?q={city}&appid=5396a21d4a839c7acb27a1c9412de39b&units=metric"
+
+    location = "bangalore"
+
+    r =  requests.get(url=url)
+    data = r.json()
+    temp = data['main']['temp']
+    print(temp)
+    temp_data =  {"temp" : data['main']['temp']} 
+    return render_template("index.html", data = temp_data )
+
+@app.route('/log_in') #log in 
+def log_in():
+    return "log inn"
+
+if __name__ =='__main__':  
+    app.run(debug = True)  
